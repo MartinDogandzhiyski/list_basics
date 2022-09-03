5538fe67dd2284662ea5fec9f620c9702f394603
Martin Dogandzhiyski <94994594+MartinDogandzhiyski@users.noreply.github.com>
2022-09-03 21:57:56 +0300

    Add files via upload

---
+++ flattering_matrix.py
@@ -0,0 +1,15 @@
+
+
+def read_matrix():
+    n = int(input())
+
+    matrix = []
+
+    for i in range(n):
+        row = [int(x) for x in input().split(', ')]
+        matrix.append(row)
+    return matrix
+matrix = read_matrix()
+print(
+    [x for row in matrix for x in row]
+)
